454bf87a3317e45db1b6153af30d6c6399b1bd23
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-06-23 17:53:03 -0700

    added javascript and image toggle

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 // import functions
 
 // reference needed DOM elements
-
+const cat = document.getElementById('cat');
+const catSound = document.getElementById('cat-sound');
+const dog = document.getElementById('dog');
+const dogSound = document.getElementById('dog-sound');
+const horse = document.getElementById('horse');
+const horseSound = document.getElementById('horse-sound');
+const horseBottom = document.getElementById('horse-bottom');
 
 // set event listeners 
+
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
+cat.addEventListener('click', ()=>{catSound.play();
+});
+
+dog.addEventListener('click', ()=>{dogSound.play();
+
+});
+
+horse.addEventListener('click', ()=>{
+  horseSound.play();
+  horseBottom.classList.remove('hide');
+  horse.classList.add('hide');
+});
+
